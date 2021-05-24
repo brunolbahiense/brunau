78825c20aa8231cadb8b51fa7bb3cccca469b0ad
Bruno Luis Bahiense de Sousa Almeida <47977354+brunolbahiense@users.noreply.github.com>
2021-05-24 12:18:52 -0300

    first commit

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Title></S.Title>
+    <S.Description>
+      Total de dias de Brunau SEM apertar a mente de alguém: 0 
+    </S.Description>
+  </S.Wrapper>
+)
+
+export default Main
