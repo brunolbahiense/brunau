7ff9cbd8a31c2af21c7dd3b4c81e85c13c481d23
Bruno Luis Bahiense de Sousa Almeida <47977354+brunolbahiense@users.noreply.github.com>
2021-05-31 10:55:57 -0300

    img added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,29 +2,23 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import GlobalStyles from 'styles/global'
-import NextNprogress from 'nextjs-progressbar'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Brunau</title>
-        <link rel="shortcut icon" href="/img/logo.jpg" />
-        <link rel="apple-touch-icon" href="/img/logo.jpg" />
+        <link rel="shortcut icon" href="/img/icon.png" />
+        <link rel="apple-touch-icon" href="/img/icon.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
+        <meta property="og:image" content="/img/icon.png"/>
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="Se prepare, a apertação só está começando!"
         />
       </Head>
       <GlobalStyles />
-      <NextNprogress
-        color="#dd1414"
-        startPosition={0.3}
-        stopDelayMs={200}
-        height={3}
-      />
       <Component {...pageProps} />
     </>
   )
