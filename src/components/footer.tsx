78d177c8b7fa19fe1f995a57b1c3f988919d4aba
Brunolbahiense <brunoluisbahiense@gmail.com>
2021-09-21 19:56:01 -0300

    footer updated

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,13 @@
-import * as S from '../styles/styles'
+import Link from 'next/link'
+import * as S from '../styles/footer'
 
 const Footer = () => (
-  <>
-    <S.Footer>teste</S.Footer>
-  </>
+  <S.Footer>
+    <S.Description> Desenvolvido por </S.Description>
+    <Link href="https://bluis.vercel.app/">
+      <S.Bluis src="img/bluis.svg" alt="Logotipo da empresa escrito Bluis" />
+    </Link>
+  </S.Footer>
 )
 
 export default Footer
